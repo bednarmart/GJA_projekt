7b5a6ea1f7d4c705783e7e5e4763c90e2ed26d8f
Martin <Martin@192.168.137.191>
2013-05-18 00:04:20 +0200

    Add user registration

--- src/java/net/gjashop/actions/UserAction.java
+++ src/java/net/gjashop/actions/UserAction.java
@@ -76,6 +76,51 @@ public class UserAction extends ActionSupport {
         return SUCCESS;
     }
     
+    public String createUser () throws Exception
+    {
+        boolean errIn = false;
+        if (isInvalid(user.getSurname())) {
+            addActionError("Chybí příjmení");
+            errIn = true;
+        }
+        if (isInvalid(user.getName())) {
+            addActionError("Chybí jméno");
+            errIn = true;
+        }
+        if (isInvalid(user.getLogin())) {
+            addActionError("Chybí login");
+            errIn = true;
+        }
+        if (isInvalid(user.getPass())) {
+            addActionError("Chybí heslo");
+            errIn = true;
+        }
+        if (errIn) return INPUT;
+        
+        try{
+            dbProvider.getSession().clear();
+            dbProvider.getSession().beginTransaction();
+            
+            dbProvider.createUser(user.getName(),
+                                  user.getSurname(),
+                                  user.getLogin(),
+                                  user.getPass(),
+                                  0);
+            dbProvider.getSession().getTransaction().commit();
+        }
+        
+        catch(Exception e){
+           System.out.println(e);    
+           addActionError("Problem s databazi");
+           return INPUT;
+        }
+        
+        this.login = user.getLogin();
+        this.pass = user.getPass();
+        
+        return this.doLogin();
+    }
+    
     private boolean isInvalid(String value) {
         return (value == null || value.length() == 0);
     }
---
+++ web/user/Registration.jsp
@@ -0,0 +1,45 @@
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@ taglib prefix="s" uri="/struts-tags" %>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Prodej oblečení</title>
+        <link rel="stylesheet" type="text/css" href="<%= request.getContextPath()%>/shop/css/shopping.css" />
+        <script type="text/javascript" src="<%= request.getContextPath()%>/script/shop.js"></script>
+    </head>
+    
+    <body onload="init('<%= request.getContextPath()%>');">
+        <div id="page">
+            <%-- Hlava --%>
+            <div id="head">
+                <span><h2>Registrace</h2></span>
+         
+                <span class="head_menu">
+                    
+                </span>
+            </div>
+            <s:actionerror/>
+            
+            <s:form action="createUser" >
+              <s:textfield name="user.name" label="Jméno" value="" />
+              <s:textfield name="user.surname" label="Příjmení" value="" />
+              <s:textfield name="user.login" label="Login" value="" />
+              <s:textfield name="user.pass" label="Heslo" value="" />
+              <s:submit value="Registrace" align="center" />
+            </s:form>
+    
+     <%-- Pata --%>
+            <div id="footer">
+                Autoři: Radim Reš, Vojtěch Škorvaga, Martin Bednář
+            </div>
+        </div>
+        
+        <%-- Formulář k mapování dodatečných parametrů příchozích z AJAXu --%>
+        
+        <form id="formRequest">
+            <input type="hidden" name="state" value="">
+        </form>
+    </body>
+</html>
+
+
